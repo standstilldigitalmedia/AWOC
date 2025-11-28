7a1ce7f88282ce6c6b3967cc207c8370f788ef76
standstilldigitalmedia <93889983+standstilldigitalmedia@users.noreply.github.com>
2025-11-28 03:32:31 -0500

    SlotListManagement
    SlotListManagement has been added.

---
+++ addons/AWOC/editor/UI_elements/list_management/slot_list_management/slot_list_management.gd
@@ -0,0 +1,12 @@
+@tool
+class_name AWOCSlotListManagement
+extends AWOCListManagementList
+
+
+func populate_control() -> void:
+	populate_control_with_type(AWOCResourceType.Type.SLOT, AWOCEditorGlobal.plugin_path.path_join(AWOCEditorGlobal.SLOT_ROW_PATH))
+
+
+func _on_resource_modified(resource_type: AWOCResourceType.Type, result: String) -> void:
+	if resource_type == AWOCResourceType.Type.SLOT:
+		set_resource_modified(result)
